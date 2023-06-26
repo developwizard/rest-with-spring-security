2637a260a0a91034edb73c6b2a9cb36550a5fad7
dewiz <churkin.it@gmail.com>
2023-06-26 22:58:54 +0300

    Initial commit

---
+++ src/main/java/com/dewiz/restwithspringsecurity/RestWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.dewiz.restwithspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dewiz/restwithspringsecurity/RestWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dewiz.restwithspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
