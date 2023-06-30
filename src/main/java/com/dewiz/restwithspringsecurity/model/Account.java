d7da237b25b14b3b4e98ae5d224691d561c4b7d2
dewiz <churkin.it@gmail.com>
2023-06-30 21:24:45 +0300

    Added Account model and repository

---
+++ src/main/java/com/dewiz/restwithspringsecurity/model/Account.java
@@ -0,0 +1,34 @@
+package com.dewiz.restwithspringsecurity.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private boolean credentrialsexpired = false;
+    private boolean expired = false;
+    private boolean locked = false;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "AccountRole",
+            joinColumns = @JoinColumn(name = "accountId", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roleId", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+}
