cf5fdbebd30ed5c4cff2901596dc4dba04e81818
dewiz <churkin.it@gmail.com>
2023-07-02 21:02:11 +0300

    Fix: Exclude password from JSON

--- src/main/java/com/dewiz/restwithspringsecurity/model/Account.java
+++ src/main/java/com/dewiz/restwithspringsecurity/model/Account.java
@@ -1,5 +1,6 @@
 package com.dewiz.restwithspringsecurity.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class Account {
     private Long id;
     @Column(unique = true)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private boolean enabled = true;
     private boolean credentrialsexpired = false;
